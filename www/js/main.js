bff5b0879b4ecfcc21e8f0c49ce1c086c3a997c1
Fabricio Zuardi <fabricio@fabricio.org>
2013-02-23 01:48:49 -0300

    time signature selector dialog

--- www/js/main.js
+++ www/js/main.js
@@ -7,6 +7,9 @@ require(["lib/pulse_generator"], sound_init);
   var bpm_el,
       tempo_mark_el,
       time_signature_el,
+      time_signature_button_el,
+      time_signature_dialog_el,
+      time_signature_submit_button_el,
       mute_button_el,
       play_pause_button_el,
       decrease_speed_button_el,
@@ -59,39 +62,75 @@ function setSpeed(bpm_value){
   bpm_el.textContent = bpm_value;
   tempo_mark_el.textContent = bpmToTempoMark(bpm_value);
 }
-function increaseSpeed(event){
-  setSpeed(++bpm_speed);
-}
-function decreaseSpeed(event){
-  setSpeed(--bpm_speed);
-}
-function setTimeSignature(time_signature_value){
-  time_signature = time_signature_value;
-}
-function muteSoundToggle(){
-  audioDestination.volume = (audioDestination.volume + 1) % 2;
-}
 function startMetronome(){
   start();
 }
 function stopMetronome(){
   clearInterval(buffer_interval);
+  buffer_interval = undefined;
+}
+function setTimeSignature(time_signature_value){
+  time_signature = time_signature_value;
+  time_signature_el.textContent = time_signature_value;
+  time_signature_button_el.textContent = time_signature_value;
+}
+function increaseSpeed(event){
+  event.preventDefault();
+  event.stopPropagation();
+  setSpeed(++bpm_speed);
+}
+function decreaseSpeed(event){
+  event.preventDefault();
+  event.stopPropagation();
+  setSpeed(--bpm_speed);
+}
+function muteSoundToggle(event){
+  event.preventDefault();
+  event.stopPropagation();
+  audioDestination.volume = (audioDestination.volume + 1) % 2;
 }
 function playPauseClicked(event){
+  console.log('playPauseClicked');
+  event.preventDefault();
+  event.stopPropagation();
   if (event.target.className === 'disabled') { return false; }
-  if (event.target.dataset.action === 'play'){
-    event.target.dataset.action = 'stop';
+  console.log(buffer_interval);
+  if (typeof buffer_interval === 'undefined'){
     startMetronome();
   } else {
-    event.target.dataset.action = 'play';
     stopMetronome();
   }
+}
+function openTimeSignatureDialog(event){
+  event.preventDefault();
+  event.stopPropagation();
+  time_signature_dialog_el.style.display = "inline-block";
+}
+function timeSignatureDialogSubmit(event){
+  event.preventDefault();
+  event.stopPropagation();
+  for (var i=0; i < time_signature_dialog.elements['option'].length; i++){
+      if (time_signature_dialog.elements['option'][i].checked){
+        setTimeSignature(time_signature_dialog.elements['option'][i].value);
+        time_signature_dialog_el.style.display = "none";
+        return false;
+      }
+  }
+}
+function timeSignatureDialogCancel(event){
+  event.preventDefault();
+  event.stopPropagation();
+  time_signature_dialog_el.style.display = "none";
 }
 function domReady(doc){
   //assign elements
   bpm_el = doc.querySelector("#bpm");
   tempo_mark_el = doc.querySelector("#tempo_mark");
   time_signature_el = doc.querySelector("#time_signature");
+  time_signature_button_el = doc.querySelector("#time_signature_button");
+  time_signature_dialog_el = doc.querySelector("#time_signature_dialog");
+  time_signature_cancel_button_el = doc.querySelector("#time_signature_cancel_button");
+  time_signature_submit_button_el = doc.querySelector("#time_signature_submit_button");
   mute_button_el = doc.querySelector("#mute_button");
   play_pause_button_el = doc.querySelector("#play_pause_button");
   decrease_speed_button_el = doc.querySelector("#decrease_speed_button");
@@ -104,9 +143,19 @@ function domReady(doc){
 
   //assign events
   mute_button_el.addEventListener('click', muteSoundToggle, true);
+  mute_button_el.addEventListener('touchstart', muteSoundToggle, true);
   play_pause_button_el.addEventListener('click', playPauseClicked, true);
+  play_pause_button_el.addEventListener('touchstart', playPauseClicked, true);
   decrease_speed_button_el.addEventListener('click', decreaseSpeed, true);
+  decrease_speed_button_el.addEventListener('touchstart', decreaseSpeed, true);
   increase_speed_button_el.addEventListener('click', increaseSpeed, true);
+  increase_speed_button_el.addEventListener('touchstart', increaseSpeed, true);
+  time_signature_button_el.addEventListener('click', openTimeSignatureDialog, true);
+  time_signature_button_el.addEventListener('touchstart', openTimeSignatureDialog, true);
+  time_signature_submit_button_el.addEventListener('click', timeSignatureDialogSubmit, true);
+  time_signature_submit_button_el.addEventListener('touchstart', timeSignatureDialogSubmit, true);
+  time_signature_cancel_button_el.addEventListener('click', timeSignatureDialogCancel, true);
+  time_signature_cancel_button_el.addEventListener('touchstart', timeSignatureDialogCancel, true);
 }
 
 
